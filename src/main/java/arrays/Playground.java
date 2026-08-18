f75cfe25169ce49e75bcecce11116b624e48c7e6
VenkataSunil <venkatasunil.sana@gmail.com>
2026-08-18 10:59:43 +0530

    First commit.

---
+++ src/main/java/arrays/Playground.java
@@ -0,0 +1,11 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Playground {
+    static void main() {
+        int[] firstNumericArray = new int[]{1,2,3,4,5};
+
+        System.out.println(Arrays.toString(firstNumericArray));
+    }
+}
